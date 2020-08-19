cf5821f6ef845cd29a9b98d635a27c710d786f09
Razaul Hridoy <imrazaulhridoy@gmail.com>
2020-08-19 15:56:40 +0600

    file here

---
+++ js/main.js
@@ -0,0 +1,308 @@
+(function ($) {
+"use strict";
+
+// meanmenu
+$('#mobile-menu').meanmenu({
+	meanMenuContainer: '.mobile-menu',
+	meanScreenWidth: "992"
+});
+
+// One Page Nav
+var top_offset = $('.header-area').height() - 10;
+$('.main-menu nav ul').onePageNav({
+	currentClass: 'active',
+	scrollOffset: top_offset,
+});
+
+
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$(".header-sticky").removeClass("sticky");
+	} else {
+		$(".header-sticky").addClass("sticky");
+	}
+});
+
+
+
+// mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider-active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: false,
+		autoplaySpeed: 10000,
+		dots: false,
+		fade: true,
+		arrows: false,
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+// brand-active
+$('.brand-active').slick({
+	dots: false,
+	arrows: false,
+	infinite: true,
+	autoplay: true,
+  	autoplaySpeed: 2000,
+	slidesToShow: 6,
+	slidesToScroll: 6,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 4,
+		  slidesToScroll: 4,
+		  infinite: true,
+		  arrows: false,
+		  dots: false,
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 320,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 1
+		}
+	  }
+	]
+  });
+
+
+
+// owlCarousel
+$('.testimonial-active').owlCarousel({
+    loop:true,
+	margin:0,
+	autoplay: false,
+	items:1,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:false,
+	dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:1
+        },
+        992:{
+            items:1
+        }
+    }
+})
+
+
+$('.time-countdown').countdown({
+	date: '12/24/2020 23:59:59'
+	}, function () {
+	  alert('Merry Christmas!');
+	});
+
+
+
+// counter
+$('.count').counterUp({
+    delay: 10,
+    time: 10000,
+})
+
+
+/* magnificPopup video view */
+$('.video-popup').magnificPopup({
+	type: 'iframe'
+});
+
+
+// isotop
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		// use outer width of grid-sizer for columnWidth
+		columnWidth: '.grid-item',
+	  }
+	});
+});
+
+// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+
+
+// scrollToTop
+$.scrollUp({
+	scrollName: 'scrollUp', // Element ID
+	topDistance: '300', // Distance from top before showing element (px)
+	topSpeed: 300, // Speed back to top (ms)
+	animation: 'fade', // Fade, slide, none
+	animationInSpeed: 200, // Animation in speed (ms)
+	animationOutSpeed: 200, // Animation out speed (ms)
+	scrollText: '<i class="fas fa-arrow-up"></i>', // Text for element
+	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
+
+// WOW active
+new WOW().init();
+
+
+
+function basicmap() {
+	// Basic options for a simple Google Map
+	// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+	var mapOptions = {
+		// How zoomed in you want the map to start at (always required)
+		zoom: 11,
+		scrollwheel: false,
+		// The latitude and longitude to center the map (always required)
+		center: new google.maps.LatLng(40.6700, -73.9400), // New York
+		// This is where you would paste any style found on Snazzy Maps.
+		styles: [
+			{
+				"featureType": "administrative",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#444444"
+					}
+				]
+			},
+			{
+				"featureType": "landscape",
+				"elementType": "all",
+				"stylers": [
+					{
+						"color": "#f2f2f2"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": -100
+					},
+					{
+						"lightness": 45
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "simplified"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels.icon",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "transit",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "all",
+				"stylers": [
+					{
+						"color": "#FF5252"
+					},
+					{
+						"visibility": "on"
+					}
+				]
+			}
+		]
+	};
+	// Get the HTML DOM element that will contain your map 
+	// We are using a div with id="map" seen below in the <body>
+	var mapElement = document.getElementById('contact-map');
+
+	// Create the Google Map using our element and options defined above
+	var map = new google.maps.Map(mapElement, mapOptions);
+
+	// Let's also add a marker while we're at it
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(40.6700, -73.9400),
+		map: map,
+		title: 'Cryptox'
+	});
+}
+if ($('#contact-map').length != 0) {
+	google.maps.event.addDomListener(window, 'load', basicmap);
+}
+
+
+})(jQuery);
